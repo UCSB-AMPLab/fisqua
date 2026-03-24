17d3fdbf5a32234d0d9b471f1ba3cb3873c87e1c
Juan Cobo Betancourt <juan@neogranadina.org>
2026-03-24 16:13:19 -0700

    Remove orphaned items route

--- app/routes/_auth.projects.$id.items.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router";
-
-export function loader({ params }: { params: { id: string } }) {
-  throw redirect(`/projects/${params.id}/volumes`);
-}
-
-export default function ProjectItems() {
-  return null;
-}
